b7878b00942410463d8b5b28bbda982ae0cfcdbd
Marti Merisalu <marti.merisalu@tktk.ee>
2023-05-07 21:05:37 +0300

    add week3 code

---
+++ OddorEven/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int result = userNum % 2;
+
+if(result != 0) //ei ole võrdne !=, on võrne ==
+{
+    Console.WriteLine("USer number is odd.");
+
+}
+else
+{
+    Console.WriteLine("User number is even");
+}
---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);  // väärtus 0-6
+
+switch (weekDay)    // väärtus, mida kontrollitakse
+{
+    case 0:     //0 võrdub if(weekday == 0)
+        Console.WriteLine("It's Sunday!");
+        break;
+        case 1:
+        Console.WriteLine("It's Monday!");
+        break;
+        case 2:
+        Console.WriteLine("Its Tuesday!");
+        break;
+        case 3:
+        Console.WriteLine("Its Wednesday!");
+        break;
+        case 4:
+        Console.WriteLine("Its Thursday!");
+        break;
+        case 5:
+        Console.WriteLine("Its Friday!");
+        break;
+        case 6:
+        Console.WriteLine("Its Saturday!");
+        break;
+        default:
+        Console.WriteLine("Oops, kalender läks katki");
+        break;
+}
+Console.WriteLine("Have a nice day!");
